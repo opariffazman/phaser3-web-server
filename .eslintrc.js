e5c63d7402d60dd08576bec545cb3c54a7af252a
opariffazman <ariff.azman@proton.me>
2023-09-08 23:16:08 +0800

    feat: initalized nodejs dev env

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    node: true,
+    browser: true,
+    commonjs: true,
+    es2021: true
+  },
+  extends: ['eslint:recommended', 'prettier'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+    'no-unused-vars': 'warn',
+    camelcase: ['error', { properties: 'always' }]
+  }
+}
